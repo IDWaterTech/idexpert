086d068213a83d454e781480d0f73ece8790417e
wei10cool <x215033@yahoo.com.tw>
2021-07-15 13:46:16 +0800

    廠區設定修改前預先完備程式

--- pages/factory.vue
+++ pages/factory.vue
@@ -14,7 +14,11 @@
               clearable
               @change="
                 () => {
-                  if(sel_main){}else{sel_area = undefined;pooldata=[];};
+                  if (sel_main) {
+                  } else {
+                    sel_area = undefined;
+                    pooldata = [];
+                  }
                 }
               "
               dense
@@ -22,10 +26,21 @@
             </v-select
           ></v-col>
           <v-col cols="6">
-            <v-btn icon color="success"
+            <v-btn icon color="success" @click="showdialog('add', 'main')"
               ><v-icon>mdi-plus</v-icon></v-btn
             >
-            <v-btn icon :disabled="!sel_main" color="error"
+            <v-btn
+              icon
+              color="success"
+              :disabled="!sel_main"
+              @click="showdialog('edit', 'main')"
+              ><v-icon>mdi-pencil-outline</v-icon></v-btn
+            >
+            <v-btn
+              icon
+              :disabled="!sel_main"
+              color="error"
+              @click="delsubmit('main', maindata[sel_main - 1].name)"
               ><v-icon>mdi-delete</v-icon></v-btn
             >
           </v-col>
@@ -49,10 +64,25 @@
             ></v-select>
           </v-col>
           <v-col cols="6">
-            <v-btn icon :disabled="!sel_main" color="success"
+            <v-btn
+              icon
+              :disabled="!sel_main"
+              color="success"
+              @click="showdialog('add', 'area')"
               ><v-icon>mdi-plus</v-icon></v-btn
             >
-            <v-btn icon :disabled="!sel_area" color="error"
+            <v-btn
+              icon
+              color="success"
+              :disabled="!sel_area"
+              @click="showdialog('edit', 'area')"
+              ><v-icon>mdi-pencil-outline</v-icon></v-btn
+            >
+            <v-btn
+              icon
+              :disabled="!sel_area"
+              color="error"
+              @click="delsubmit('area', areadata[sel_area - 1].name)"
               ><v-icon>mdi-delete</v-icon></v-btn
             >
           </v-col>
@@ -76,7 +106,11 @@
             <v-btn icon :disabled="!sel_area" color="success"
               ><v-icon>mdi-plus</v-icon></v-btn
             >
-            <v-btn icon :disabled="!sel_area" color="error"
+            <v-btn
+              icon
+              :disabled="!sel_pool"
+              color="error"
+              @click="delsubmit('pool', pooldata[sel_pool - 1].name)"
               ><v-icon>mdi-delete</v-icon></v-btn
             >
           </v-col>
@@ -85,9 +119,9 @@
       <v-spacer></v-spacer>
     </v-row>
     <v-row>
-        <v-col>
-            <v-btn color="primary">池基本參數設定</v-btn>
-        </v-col>
+      <v-col>
+        <v-btn color="primary">池基本參數設定</v-btn>
+      </v-col>
     </v-row>
     <v-row>
       <v-col
@@ -105,21 +139,51 @@
           <v-divider></v-divider>
           <v-card-text>
             <ul>
-              <li v-for="it in Object.keys(item)" :key="it">{{ it }}:{{ item[it] }}</li>
+              <li v-for="it in Object.keys(item)" :key="it">
+                {{ it }}:{{ item[it] }}
+              </li>
             </ul>
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
 
-
-
-    <v-dialog v-model="dialog.main"></v-dialog>
+    <v-dialog v-model="dialog.main" width="500px">
+      <v-card>
+        <v-card-title
+          >{{
+            edititem.type == "add"
+              ? "新增"
+              : edititem.type == "edit"
+              ? "編輯"
+              : "刪除"
+          }}-{{
+            edititem.class == "main"
+              ? "廠"
+              : edititem.class == "area"
+              ? "區域"
+              : "養殖池"
+          }}</v-card-title
+        >
+        <v-card-text>
+          <v-text-field
+            placeholder="請輸入名稱"
+            v-model="edititem.value"
+            autocomplete="off"
+          ></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="editsubmit">送出</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
 import https from "https";
+import { log } from "util";
 const agent = new https.Agent({
   rejectUnauthorized: false
 });
@@ -132,8 +196,13 @@ export default {
       sel_area: "",
       sel_pool: "",
       pooldata: [],
-      dialog:{
-          main:false
+      dialog: {
+        main: false
+      },
+      edititem: {
+        type: "add", //add新增還是del刪除
+        class: "", //新增廠、區、池
+        value: ""
       }
     };
   },
@@ -142,10 +211,15 @@ export default {
       .get("https://61.56.172.10/architecture/", { httpsAgent: agent })
       .then(res => {
         this.maindata = res.data;
+        console.log("API:" + res.request.responseURL);
+      })
+      .catch(error => {
+        this.$axios.error("error:" + error, { duration: 2000 });
       });
   },
   methods: {
     getPoolData: async function() {
+      this.sel_pool = "";
       let pool = [];
       if (!this.sel_area) {
         this.pooldata = pool;
@@ -162,7 +236,7 @@ export default {
           { httpsAgent: agent }
         )
         .then(res => {
-          console.log(res.data);
+          console.log("API:" + res.request.responseURL);
           pool = res.data;
         })
         .catch(error => {
@@ -172,6 +246,46 @@ export default {
         .finally(() => {
           /* 不論失敗成功皆會執行 */ this.pooldata = pool;
         });
+    },
+    editsubmit: async function() {
+      console.log("edit submit", this.edititem);
+    },
+    showdialog: function(data, location) {
+      this.edititem.type = data; //add edit
+      this.edititem.class = location; //main area pool
+      this.edititem.id = this.sel_main;
+      switch (location) {
+        case "main":
+          this.edititem.value =
+            data == "add"
+              ? ""
+              : data == "edit"
+              ? this.maindata.filter(x => x.id == this.sel_main)[0].name
+              : ""; //add 空 del：帶項目值
+          break;
+        case "area":
+          this.edititem.value =
+            data == "add"
+              ? ""
+              : data == "edit"
+              ? this.areadata.filter(x => x.id == this.sel_area)[0].name
+              : ""; //add 空 del：帶項目值
+          break;
+        default:
+          break;
+      }
+
+      this.dialog.main = true;
+    },
+    delsubmit: async function(location, value) {
+      if (confirm(`是否刪除${value}`)) {
+        let parm = {
+          class: location, //廠、區、水池
+          value: value //研發一廠
+        };
+        console.log(parm);
+        this.$toast.success(`修改成功`, { duration: 2000 });
+      }
     }
   },
   computed: {
