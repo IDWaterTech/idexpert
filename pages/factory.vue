9425342f9c9c10d20cd449d06d710b4771863b9f
wei10cool <x215033@yahoo.com.tw>
2021-08-26 10:42:57 +0800

    水池新增修改完成

--- pages/factory.vue
+++ pages/factory.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>廠域設定(開發中)</h2>
+    <h2>廠域設定</h2>
     <v-row no-gutters>
       <v-col cols="12" sm="4">
         <v-row no-gutters align="center">
@@ -115,25 +115,36 @@
             ></v-autocomplete>
           </v-col>
           <v-col cols="6">
-            <v-btn icon :disabled="!sel_area" color="success"
+            <v-btn
+              icon
+              :disabled="!sel_area"
+              color="success"
+              @click="showdialog_pool('add')"
               ><v-icon>mdi-plus</v-icon></v-btn
             >
             <v-btn
               icon
               :disabled="!sel_pool"
+              color="success"
+              @click="showdialog_pool('edit')"
+              ><v-icon>mdi-pencil-outline</v-icon></v-btn
+            >
+            <v-btn
+              icon
+              :disabled="!sel_pool"
               color="error"
-              @click="delsubmit('pool', pooldata[sel_pool - 1].name)"
+              @click="
+                delsubmit(
+                  'pool',
+                  pooldata.filter(x => x.id == sel_pool)[0].name
+                )
+              "
               ><v-icon>mdi-delete</v-icon></v-btn
             >
           </v-col>
         </v-row>
       </v-col>
       <v-spacer></v-spacer>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-btn color="primary">池基本參數設定</v-btn>
-      </v-col>
     </v-row>
     <v-row>
       <v-col
@@ -150,57 +161,165 @@
           </v-card-subtitle>
           <v-divider></v-divider>
           <v-card-text>
-            <ul>
-              <li v-for="it in Object.keys(item)" :key="it">
-                {{ it }}:{{ item[it] }}
-              </li>
-            </ul>
+            <ol>
+              <div v-for="it in Object.keys(item)" :key="it">
+                <li
+                  v-if="itemname.filter(x => x.name == it)[0].visible == true"
+                  style="font-weight:500;"
+                >
+                  {{ itemname.filter(x => x.name == it)[0].text }}-{{
+                    item[it]
+                  }}
+                </li>
+              </div>
+            </ol>
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
 
     <v-dialog v-model="dialog.main" width="500px">
-      <v-card>
-        <v-card-title
-          >{{
-            edititem.type == "add"
-              ? "新增"
-              : edititem.type == "edit"
-              ? "編輯"
-              : "刪除"
-          }}-{{
-            edititem.class == "main"
-              ? "廠"
-              : edititem.class == "area"
-              ? "區域"
-              : "養殖池"
-          }}</v-card-title
-        >
-        <v-card-text>
-          <v-text-field
-            placeholder="請輸入名稱"
-            v-model="edititem.value"
-            autocomplete="off"
-          ></v-text-field>
-          <v-text-field
-            v-if="edititem.class == 'area'"
-            placeholder="請輸入英文代稱(wc、tf、zw...)"
-            v-model="edititem.value_en"
-            autocomplete="off"
-          ></v-text-field>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" @click="editsubmit">送出</v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-form ref="mainform" v-model="mainvalid" lazy-validation>
+        <v-card>
+          <v-card-title
+            >{{
+              edititem.type == "add"
+                ? "新增"
+                : edititem.type == "edit"
+                ? "編輯"
+                : "刪除"
+            }}-{{
+              edititem.class == "main"
+                ? "廠"
+                : edititem.class == "area"
+                ? "區域"
+                : "養殖池"
+            }}</v-card-title
+          >
+          <v-card-text>
+            <v-text-field
+              placeholder="請輸入名稱"
+              v-model="edititem.value"
+              :rules="rules.require"
+              autocomplete="off"
+            ></v-text-field>
+            <v-text-field
+              v-if="edititem.class == 'area'"
+              placeholder="請輸入英文代稱(wc、tf、zw...)"
+              v-model="edititem.value_en"
+              :rules="rules.require"
+              autocomplete="off"
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="editsubmit">送出</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </v-dialog>
+    <v-dialog v-model="dialog.pool" width="500px">
+      <v-form ref="poolform" v-model="poolvalid" lazy-validation>
+        <v-card v-if="sel_area">
+          <v-card-title
+            >{{ areadata.filter(x => x.id == sel_area)[0].name }}-養殖池-{{
+              edititem_pool.type == "add" ? "新增" : "編輯"
+            }}</v-card-title
+          >
+          <v-divider></v-divider>
+          <v-card-text>
+            <v-text-field
+              autocomplete="off"
+              v-model="edititem_pool.parm.name"
+              :rules="rules.require"
+              clearable
+              dense
+            >
+              <template slot="prepend"
+                ><span style="width:50px;">名稱</span></template
+              >
+            </v-text-field>
+            <v-text-field
+              autocomplete="off"
+              v-model="edititem_pool.parm.volume"
+              :rules="rules.require"
+              type="number"
+              clearable
+            >
+              <template slot="prepend"
+                ><span style="width:50px;">體積</span></template
+              >
+            </v-text-field>
+            <v-text-field
+              autocomplete="off"
+              v-model="edititem_pool.parm.depth"
+              :rules="rules.require"
+              type="number"
+              clearable
+            >
+              <template slot="prepend"
+                ><span style="width:50px;">深度</span></template
+              >
+            </v-text-field>
+            <!-- <v-text-field
+              autocomplete="off"
+              v-model="edititem_pool.parm.density"
+              :rules="rules.require"
+              clearable
+            >
+              <template slot="prepend"
+                ><span style="width:50px;">密度</span></template
+              >
+            </v-text-field> -->
+            <v-text-field
+              autocomplete="off"
+              v-model="edititem_pool.parm.num"
+              :rules="rules.require"
+              type="number"
+              clearable
+            >
+              <template slot="prepend"
+                ><span style="width:50px;">池子數</span></template
+              >
+            </v-text-field>
+            <v-text-field
+              autocomplete="off"
+              v-model="edititem_pool.parm.aeration_tray_num"
+              :rules="rules.require"
+              type="number"
+              clearable
+            >
+              <template slot="prepend"
+                ><span style="width:50px;">曝氣盤數</span></template
+              >
+            </v-text-field>
+            <v-select
+              v-model="edititem_pool.parm.pond_state_id"
+              :items="poolstat"
+              :rules="rules.require"
+              item-text="name"
+              item-value="id"
+              :disabled="true"
+              v-if="edititem_pool.type == `add`"
+              ><template slot="prepend"
+                ><span style="width:50px;">狀態</span></template
+              ></v-select
+            >
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="poolsubmit(edititem_pool.type)">送出</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
     </v-dialog>
   </div>
 </template>
 
 <script>
 import https from "https";
+import _ from "lodash";
 const agent = new https.Agent({
   rejectUnauthorized: false
 });
@@ -210,24 +329,43 @@ export default {
   middleware: "auth",
   data() {
     return {
+      rules: { require: [v => !!v || "*必要項目"] },
       maindata: [],
       sel_main: "",
       sel_area: "",
       sel_pool: "",
       pooldata: [],
       dialog: {
-        main: false
+        main: false,
+        pool: false
       },
       edititem: {
         type: "add", //add新增還是del刪除
         class: "", //新增廠、區、池
         value: ""
-      }
+      },
+      edititem_pool: {
+        type: "add", //add新增還是del刪除
+        parm: {}
+      },
+      poolstat: [],
+      poolvalid: true,
+      mainvalid: true,
+      itemname: [
+        { name: "id", text: "id", visible: false },
+        { name: "name", text: "名稱", visible: true },
+        { name: "volume", text: "體積", visible: true },
+        { name: "depth", text: "深度", visible: true },
+        { name: "num", text: "池子數", visible: true },
+        { name: "aeration_tray_num", text: "曝氣盤數", visible: true },
+        { name: "state", text: "狀態", visible: true }
+      ]
     };
   },
   async created() {
-    await this._pageCheck();//驗證頁面是否可檢視
+    await this._pageCheck(); //驗證頁面是否可檢視
     await this.getmain();
+    await this.getpoolstat(); //取得池狀態清單
   },
   methods: {
     getmain: async function() {
@@ -239,6 +377,17 @@ export default {
         })
         .catch(error => {
           this.$axios.error("error:" + error, { duration: 2000 });
+        });
+    },
+    getpoolstat: async function() {
+      //取得池狀態清單
+      await this.$axios
+        .get("https://61.56.172.10/pond-state/")
+        .then(res => {
+          this.poolstat = res.data.filter(x => x.name != ""); //不提供保留項;
+        })
+        .catch(error => {
+          alert("error:" + error.message);
         });
     },
     getPoolData: async function() {
@@ -271,6 +420,9 @@ export default {
         });
     },
     editsubmit: async function() {
+      if (this.$refs.mainform.validate() == false) {
+        return;
+      }
       const updUser = this.$auth.$state.user.email;
       console.log(this.edititem);
       let id = this.edititem.id;
@@ -354,7 +506,7 @@ export default {
               });
             break;
           case "area":
-             parm = {
+            parm = {
               area_no: this.edititem.value_en,
               name: this.edititem.value,
               updated_user: updUser
@@ -369,7 +521,7 @@ export default {
                   this.dialog.main = false;
                   this.getmain();
                   this.sel_main = "";
-                  this.sel_area="";
+                  this.sel_area = "";
                   this.$toast.success("修改成功", { duration: 2000 });
                 } else {
                   this.$toast.error("修改失敗:" + res.data, { duration: 2000 });
@@ -387,7 +539,7 @@ export default {
     showdialog: function(data, location) {
       this.edititem.type = data; //add edit
       this.edititem.class = location; //main area pool
-      
+
       switch (location) {
         case "main":
           this.edititem.id = this.sel_main;
@@ -406,14 +558,38 @@ export default {
               : data == "edit"
               ? this.areadata.filter(x => x.id == this.sel_area)[0].name
               : ""; //add 空 del：帶項目值
-              //英文代稱
-            this.edititem.value_en = (data == "add")?"": (data == "edit")? this.areadata.filter(x => x.id == this.sel_area)[0].area_no:"";
+          //英文代稱
+          this.edititem.value_en =
+            data == "add"
+              ? ""
+              : data == "edit"
+              ? this.areadata.filter(x => x.id == this.sel_area)[0].area_no
+              : "";
           break;
         default:
           break;
       }
 
       this.dialog.main = true;
+    },
+    showdialog_pool: function(data) {
+      //data is add or edit
+      this.edititem_pool.type = data;
+      this.edititem_pool.parm = {};
+      this.edititem_pool.parm.pond_area_id = this.sel_area;
+      this.edititem_pool.parm.pond_state_id = 3; //預設狀態=空池，id=3
+      if (data == "edit") {
+        var pool = this.pooldata.filter(x => x.id == this.sel_pool)[0];
+        this.edititem_pool.parm = _.cloneDeep(pool);
+        // this.edititem_pool.parm.pond_state_id = 3;
+        delete this.edititem_pool.parm.state;//不需要的項目
+        delete this.edititem_pool.parm.pond_state_id;//水池狀態不在這修改
+        for (const key in this.edititem_pool.parm) {
+          var getvalue = this.edititem_pool.parm[key];
+          this.edititem_pool.parm[key] =(typeof(getvalue)=="number" || key=="name")?getvalue: getvalue.match(/^[\d\.]+/)[0];
+        }
+      }
+      this.dialog.pool = true;
     },
     delsubmit: async function(location, value) {
       if (confirm(`是否刪除 ${value}`)) {
@@ -448,7 +624,11 @@ export default {
               this.getmain();
               this.sel_main = "";
               this.sel_area = "";
-              this.sel_pool = "";
+
+              if (location == "pool") {
+                this.sel_pool = "";
+                this.getPoolData(); //重取得清單
+              }
               this.$toast.success("刪除成功", { duration: 2000 });
             } else {
               this.$toast.error("刪除失敗:" + res.data, { duration: 2000 });
@@ -458,6 +638,51 @@ export default {
             this.$toast.error("error:" + error, { duration: 2000 });
           });
       }
+    },
+    poolsubmit: async function(data) {
+      const user = this.$auth.$state.user.email;
+      debugger;
+      if (this.$refs.poolform.validate()) {
+        if (data == "add") {//新增池
+          this.edititem_pool.parm.created_user = user;
+          var parm = this.edititem_pool.parm;
+          await this.$axios
+            .post("https://61.56.172.10/pond/", parm)
+            .then(res => {
+              console.log("API:" + res.request.responseURL);
+              if (res.data == "新增成功") {
+                this.getPoolData(); //重取得養殖池資料
+                this.dialog.pool = false; //close dialog
+                this.$toast.success(`新增成功`, { duration: 2000 });
+              } else {
+                alert("新增失敗!：" + res.data);
+              }
+            })
+            .catch(error => {
+              this.$axios.error("error:" + error, { duration: 2000 });
+            });
+        } else {//編輯池
+               this.edititem_pool.parm.updated_user = user;
+               const id = this.edititem_pool.parm.id;
+               var parm = this.edititem_pool.parm;
+               await this.$axios
+            .patch(`https://61.56.172.10/pond/${id}`, parm)
+            .then(res => {
+              console.log("API:" + res.request.responseURL);
+              if (res.data == "修改成功") {
+                this.getPoolData(); //重取得養殖池資料
+                this.dialog.pool = false; //close dialog
+                this.$toast.success(`修改成功`, { duration: 2000 });
+              } else {
+                alert("修改失敗!：" + res.data);
+              }
+            })
+            .catch(error => {
+              this.$axios.error("error:" + error, { duration: 2000 });
+            });
+        }
+      }
+      console.log(this.edititem_pool.parm);
     }
   },
   computed: {
@@ -476,7 +701,7 @@ export default {
       let area = [];
       filtermain.forEach(function(x) {
         x.node.forEach(function(y) {
-          let yitem = { id: y.id, name: y.name,area_no:y.area_no };
+          let yitem = { id: y.id, name: y.name, area_no: y.area_no };
           if (area.indexOf(yitem) == -1) {
             //沒找到
             area.push(yitem);
