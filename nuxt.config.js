e95cc0f6b261c824fa46ea2b226bce98821a627c
wei10cool <x215033@yahoo.com.tw>
2021-12-20 14:23:10 +0800

    調整api網址

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,9 +38,9 @@ export default {
     apiIIS82: "https://192.168.50.77:82",
     apiVideo8443: "https://192.168.50.77:8443",
     internal: {
-      apiUrl: "https://localhost.idwatertech.com/api", //統一不要有後斜線
+      apiUrl: "https://www.idwatertech.com/api", //統一不要有後斜線
       apiIIS82: "https://www.idwatertech.com:82",
-      apiVideo8443: "https://localhost.idwatertech.com:8443"
+      apiVideo8443: "https://www.idwatertech.com:8443"
     },
     external: {
       apiUrl: "https://61.56.172.10/api", //統一不要有後斜線
