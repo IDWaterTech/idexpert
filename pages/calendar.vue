97c744b5501c5f9ea3c31a93647a33778d4c4fa4
wei10cool <x215033@yahoo.com.tw>
2022-01-14 16:03:50 +0800

    資料範圍更新

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -6,10 +6,17 @@
         <v-row align-content="center">
           <v-col cols="12" sm="3">
             <v-autocomplete
-              auto-select-first dark dense outlined :items="datarange" item-text="name" item-value="level" v-model="level"
+              auto-select-first
+              dark
+              dense
+              outlined
+              :items="datarange"
+              item-text="name"
+              item-value="level"
+              v-model="level"
               @change="rangechange"
             >
-            <template slot="prepend"
+              <template slot="prepend"
                 ><span style="width:70px;">資料範圍</span></template
               >
             </v-autocomplete>
@@ -21,6 +28,7 @@
               :default-expand-level="1"
               :disable-branch-nodes="true"
               children="node"
+              :multiple="true"
               :normalizer="
                 node => {
                   return { children: node.node };
@@ -29,20 +37,97 @@
               style="font-size:1.2em;"
             >
               <div slot="value-label" slot-scope="{ node }">
-                {{ `${(node.raw.parent != undefined && (node.raw.parent.length)>0)?node.raw.parent+'_':''}${node.raw.name}` }}
+                {{
+                  `${
+                    node.raw.parent != undefined && node.raw.parent.length > 0
+                      ? node.raw.parent + "_"
+                      : ""
+                  }${node.raw.name}`
+                }}
               </div>
               <div slot="option-label" slot-scope="{ node }">
                 {{ `${node.raw.name}` }}
               </div>
             </treeselect>
           </v-col>
-          <v-col cols="12" sm="3">
-            <v-btn tile dark color="primary" :disabled="!poolid">確認</v-btn>
+          <!-- 起日 -->
+          <v-col cols="12" sm="2" v-show="false">
+            <v-menu
+              v-model="menu_startdate"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              transition="scale-transition"
+              offset-y
+              min-width="auto"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="sdate"
+                  label="選擇起日"
+                  prepend-icon="mdi-calendar"
+                  readonly
+                  dense
+                  dark
+                  v-bind="attrs" :disabled="true"
+                  v-on="on"
+                  @click:prepend="
+                    () => {
+                      sdate = getNowDate();
+                    }
+                  "
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="sdate"
+                @input="menu_startdate = false"
+              ></v-date-picker>
+            </v-menu>
+          </v-col>
+          <!-- 訖日 -->
+          <v-col cols="12" sm="2" v-show="false">
+            <v-menu
+              v-model="menu_enddate"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              transition="scale-transition"
+              offset-y
+              min-width="auto"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="edate"
+                  label="選擇訖日"
+                  prepend-icon="mdi-calendar"
+                  readonly
+                  dense dark :disabled="true"
+                  v-bind="attrs"
+                  v-on="on"
+                  @click:prepend="
+                    () => {
+                      edate = getNowDate();
+                    }
+                  "
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="edate"
+                @input="menu_enddate = false"
+              ></v-date-picker>
+            </v-menu>
+          </v-col>
+          <v-col cols="12" sm="2">
+            <v-btn
+              tile
+              dark
+              color="primary"
+              :disabled="!poolidcpd || poolidcpd.length == 0"
+              @click="getEventData"
+              >確認</v-btn
+            >
           </v-col>
         </v-row>
-        
-        
       </v-col>
+      <!-- 日曆 -->
       <v-col cols="12">
         <v-sheet height="64">
           <v-toolbar flat>
@@ -124,7 +209,7 @@
                 <v-btn icon>
                   <v-icon>mdi-pencil</v-icon>
                 </v-btn>
-                <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+                <v-toolbar-title v-html="`[${selectedEvent.event_level_name}]_${selectedEvent.name}`"></v-toolbar-title>
                 <v-spacer></v-spacer>
                 <v-btn icon>
                   <v-icon>mdi-heart</v-icon>
@@ -133,21 +218,16 @@
                   <v-icon>mdi-dots-vertical</v-icon>
                 </v-btn>
               </v-toolbar>
-              <v-card-title v-if="selectedEvent.warning_level">
-                <span v-html="selectedEvent.warning_level"></span>
-              </v-card-title>
-              <v-card-title v-if="selectedEvent.created_user">
-                <span v-html="selectedEvent.created_user"></span>
-              </v-card-title>
-              <v-card-title v-if="selectedEvent.event_category_name">
-                <span v-html="selectedEvent.event_category_name"></span>
-              </v-card-title>
-              <v-card-text>
-                <span v-html="selectedEvent.details"></span>
+              <v-card-subtitle v-if="selectedEvent.start">
+                <span v-html="`起：${selectedEvent.start}<br/>訖：${selectedEvent.end}<br/>建立者：${selectedEvent.created_user}<br/>全日：${selectedEvent.timed}`"></span>
+              </v-card-subtitle>
+              <v-card-text v-if="selectedEvent.event_category_id">
+                <span v-html="`事件等級：${selectedEvent.event_level_name}<br/>事件類別：${selectedEvent.event_category_name}<br/>事件範圍：${this.selectedEvent.items.map(x=>x.name).join()}`"></span>
+                <div v-html="`事件內容：${selectedEvent.content}`"></div>
               </v-card-text>
               <v-card-actions>
-                <v-btn text color="secondary" @click="selectedOpen = false">
-                  Cancel
+                <v-btn :color="selectedEvent.color" dark tile @click="selectedOpen = false">
+                  關閉
                 </v-btn>
               </v-card-actions>
             </v-card>
@@ -170,6 +250,7 @@
 
 <script>
 import _ from "lodash";
+import dayjs from "dayjs";
 export default {
   layout: "emptynologin",
   // middleware: "auth",
@@ -190,13 +271,14 @@ export default {
       eventsData: [
         {
           id: 2,
-          started_date: "2022-01-02",
-          ended_date: "2022-01-03",
+          started_date: "2022-01-01",
+          ended_date: "2022-01-02",
           factory_id: [1],
           pond_area_id: [1],
           pond_id: [1, 2, 3],
           data_col_id: [1, 2],
-          warning_level: "03",
+          event_level_id: 1,
+          event_level_name: "重要",
           event_category_id: 4,
           event_category_name: "緊急發生",
           color: "#FF0000",
@@ -204,35 +286,49 @@ export default {
           content: "變電箱故障",
           is_all_day: true,
           created_user: "技術部-王前驊",
-          created_time: "2022-01-04 11:00:00"
+          created_time: "2022-01-04 11:00:00",
+          items:[]
         }
       ],
       colors: [
-        "grey darken-1",
-        "cyan darken-1",
+        "grey darken-1",//#7575
+        "red darken-1",//#E539
+        "cyan darken-1",//#00AC
         "teal darken-1",
         "indigo darken-2",
         "deep-purple darken-2",
         "green darken-2",
-        "blue-grey darken-1",
+        "blue-grey darken-1"
       ],
       names: [
-        "Meeting",
-        "Holiday",
+        "不重要",
+        "重要",
+        "一般",
         "PTO",
         "Travel",
         "Event",
         "Birthday",
         "Conference",
         "Party",
-        "停電",
+        "停電"
       ],
       //資料範圍
-      datarange:[{level:1,name:"廠"},{level:2,name:"區"},{level:3,name:"池"}],
+      datarange: [
+        { level: 1, name: "廠" },
+        { level: 2, name: "區" },
+        { level: 3, name: "池" }
+      ],
       level: 1,
       //treeselect
-      poolid: undefined,
+      poolid: ["研發一廠_1"],
       maindata: [],
+      //起訖日
+      sdate: dayjs(new Date())
+        .add(-10, "day")
+        .format("YYYY-MM-DD"),
+      edate: new Date().toISOString().substr(0, 10),
+      menu_startdate: false,
+      menu_enddate: false
     };
   },
   async mounted() {
@@ -241,6 +337,10 @@ export default {
     await this.getMainData();
   },
   methods: {
+    getNowDate: function() {
+      let mydate = dayjs().format("YYYY-MM-DD");
+      return mydate;
+    },
     viewDay({ date }) {
       this.focus = date;
       this.type = "day";
@@ -276,6 +376,12 @@ export default {
       nativeEvent.stopPropagation();
     },
     updateRange: async function({ start, end }) {
+      // this.sdate = start.date;
+      // this.edate = end.date;
+      console.log("updateRange!!!");
+      console.log("sdate",this.sdate);
+      console.log("edate",this.edate);
+      await this.getEventData();
       // const events = [];
       // const min = new Date(`${start.date}T00:00:00`);
       // const max = new Date(`${end.date}T23:59:59`);
@@ -301,33 +407,43 @@ export default {
     // rnd(a, b) {
     //   return Math.floor((b - a + 1) * Math.random()) + a;
     // }
-    rangechange:function(){
+    rangechange: function() {
       // var data = this.setNestedDisabled(this.maindata, "",this.level);
       // this.maindata = data;
       this.poolid = null;
     },
-    setNestedDisabled: function(obj, name,onlyshowlevel=1) {
+    setNestedDisabled: function(obj, name, onlyshowlevel = 1) {
       //全部都設成disabled
-      obj.forEach((itm, index) => {
+      var deleteindex =[];
+      var objj = obj;
+      objj.forEach((itm, index) => {
         // console.log(itm.name);//所有node(含leaf)的名稱
-        itm.parent = ((itm != undefined) && itm.hasOwnProperty("parent"))
-          ? itm.parent + "_" + name
-          : name;
-        const nodelst = ["1", "2"];//第1、2層
+        itm.parent =
+          itm != undefined && itm.hasOwnProperty("parent")
+            ? itm.parent + "_" + name
+            : name;
+        const nodelst = ["1", "2"]; //第1、2層
         if (nodelst.filter(x => x == itm.level) > 0) {
           itm.id = itm.name + "_" + itm.id;
         }
         if (itm.visible == false) {
-          //隱藏走道用
-          delete obj[index];
-          obj.splice(index, 1);//刪除 走道
-          return obj; //不用再找有無node
+        //visible的不顯示
+          deleteindex.push(index);
         }
         if (itm.hasOwnProperty("node")) {
-          this.setNestedDisabled(itm.node, itm.name,onlyshowlevel);
+          this.setNestedDisabled(itm.node, itm.name, onlyshowlevel);
         }
       });
-      return obj;
+      // for (let i = 0; i < deleteindex.length; i++) {
+      //   const element = deleteindex[i];
+      //   obj.splice(element, 1);
+      // }
+      //刪除 走道
+      for (var i = deleteindex.length -1; i >= 0; i--)
+          {
+            objj.splice(deleteindex[i],1);
+          }
+      return objj;
     },
     getMainData: async function() {
       let reqid = this.poolid;
@@ -338,9 +454,15 @@ export default {
         .get(`${this.$store.state.mydata.gobal_api.apiUrl}/architecture/`)
         .then(res => {
           this.maindata = res.data;
-          var data = this.setNestedDisabled(_.cloneDeep(this.maindata), "",this.level);
+          var data = this.setNestedDisabled(
+            _.cloneDeep(this.maindata),
+            "",
+            this.level
+          );
           this.maindata = data;
-
+        })
+        .catch(error=>{
+          this.$toast.error(`資料取得失敗:${error.message}`, { duration: 2000 });
         });
       //用id抓到name
       this.maindata.forEach(x => {
@@ -358,7 +480,7 @@ export default {
         console.log(getedItem);
       }
     },
-    nestedMain:function(obj,level){
+    nestedMain: function(obj, level) {
       obj.forEach(itm => {
         if (itm.level == level) {
           if (itm.hasOwnProperty("node")) {
@@ -370,33 +492,90 @@ export default {
         }
       });
       return obj;
+    },
+    getEventData: async function() {
+      var parms = {};
+      // started_date=2022-01-01&ended_date=2022-01-04
+      parms.started_date = this.sdate;
+      parms.ended_date = this.edate;
+      switch (this.level) {
+        case 1:
+          parms.factory_id = this.poolidcpd.join();
+          break;
+        case 2:
+          parms.pond_area_id = this.poolidcpd.join();
+          break;
+        case 3:
+          parms.pond_id = this.poolidcpd.join();
+          break;
+      }
+      await this.$axios
+        .get(`${this.$store.state.mydata.gobal_api.apiUrl}/event/`, {
+          params: parms
+        })
+        .then(res => {
+          this.eventsData = res.data;
+          console.log("event api:", res.request.responseURL);
+        })
+        .catch(err => {
+          this.$toast.error(`資料取得失敗:${err.message}`, { duration: 2000 });
+        });
     }
   },
   computed: {
-    events: function(){
+    events: function() {
       var dataa = this.eventsData;
-      let tempArray=[];
+      let tempArray = [];
       dataa.forEach(ele => {
         var item = {
           name: ele.title,
-          start:ele.started_date,
-          end:ele.ended_date,
+          start: (ele.is_all_day)?ele.started_date.substr(0,10):ele.started_date,
+          end: (ele.is_all_day)?ele.ended_date.substr(0,10):ele.started_date,
           // color: this.colors[this.rnd(0, this.colors.length - 1)],
-          color:(this.names.indexOf(ele.title)>this.colors.length)?this.colors[0]:this.colors[this.names.indexOf(ele.title)],
-          timed:ele.is_all_day,
-          details:`content：${ele.content}`,
-          warning_level:`warning_level：${ele.warning_level}`,
-          created_user:`created_user：${ele.created_user}`,
-          event_category_name:`event_category_name：${ele.event_category_name}`,
-        }
+          color: ele.color,
+          // color:
+          //   this.names.indexOf(ele.event_level_name) > this.colors.length ||
+          //   this.names.indexOf(ele.event_level_name) == -1
+          //     ? this.colors[0]
+          //     : this.colors[this.names.indexOf(ele.event_level_name)],
+          timed: ele.is_all_day,
+          content: `${ele.content}`,
+          event_level_id: `${ele.event_level_id}`,
+          event_level_name: `${ele.event_level_name}`,
+          created_user: `${ele.created_user}`,
+          event_category_id: `${ele.event_category_id}`,
+          event_category_name: `${ele.event_category_name}`,
+          items: ele.items
+        };
         tempArray.push(item);
       });
 
       return tempArray;
     },
-    maindatacpd:function(){
-      var data  = _.cloneDeep(this.maindata);
-      return this.nestedMain(data,this.level);
+    maindatacpd: function() {
+      var data = _.cloneDeep(this.maindata);
+      return this.nestedMain(data, this.level);
+    },
+    poolidcpd: function() {
+      //重組選到的id，去除_線
+      var poolid = this.poolid;
+      var poolidreg = [];
+      if (!this.poolid) {
+        return [];
+      }
+      poolid.forEach(element => {
+        //數值型態直接納入
+        if (typeof element == "number") {
+          poolidreg.push(element);
+        } else {
+          //其他自己算
+          var elereg = element.match("[^_]+$");
+          if (elereg != null && elereg != 0) {
+            poolidreg.push(elereg[0]);
+          }
+        }
+      });
+      return poolidreg;
     }
   },
   async created() {
