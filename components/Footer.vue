8d47881a7777e3a6f0b09fb1b2b04b6a2cbbdda6
wei10cool <x215033@yahoo.com.tw>
2022-04-14 17:14:20 +0800

    加入copyright

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+   v-row(align="center")
+    v-col.ma-0.pa-2(cols="12",style="font-size:0.5em;")
+      .item.text-center Copyright &copy; 2021-{{ new Date().getFullYear() }} 艾滴科技 All rights reserved.
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="sass" scoped></style>
