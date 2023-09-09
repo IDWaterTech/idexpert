784a83ee897fc8f901bea5d21ebebe0943f76d1f
wei10cool <x215033@yahoo.com.tw>
2023-09-09 16:28:12 +0800

    增加知識庫，測試站與正式站的區別

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,8 @@ export default {
     internal: {
       // apiUrl: "https://www.idwatertech.com/api", //正式站，統一不要有後斜線
       apiUrl: "https://www.idwatertech.com:8011/api", //測試站
-      apiKbUrl: "https://www.idwatertech.com:8811/api", //知識庫
+      // apiKbUrl: "https://www.idwatertech.com:8811/api", //正式站，知識庫
+      apiKbUrl: "https://www.idwatertech.com:8911/api", //測試站
       apiIIS82: "https://www.idwatertech.com:82",
       apiVideo8443: "https://www.idwatertech.com:8443"
     },
