1b46d7536ad2b244f1983da632bbec775ac6430a
hsushihya <shihya.hsu@idwater.com.tw>
2023-10-16 16:12:06 +0800

    登入頁式樣調整、首頁調整為menu card的形式

---
+++ layouts/emptychangebg.vue
@@ -0,0 +1,36 @@
+<template>
+<client-only>
+  <v-app>
+    <v-main class="bg">
+      <v-container fluid style="height: 100%;position: relative;">
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+  </client-only>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+     
+    };
+  },
+  async mounted() {
+    
+  },
+};
+</script>
+<style scoped>
+.papper {
+  background: url('~/assets/bg_blue_papper.jpg')
+    repeat center center fixed !important;
+    background-repeat: repeat !important;
+  background-size: cover;
+}
+.bg {
+  background: var(--linear-gradient-dark, linear-gradient(179deg, #00324E 0.78%, #40657A 197.21%));
+  /* background: linear-gradient(178deg, #00324E -29.51%, #40657A 140.55%); */
+}
+</style>
---
+++ layouts/emptynologin2.vue
@@ -0,0 +1,255 @@
+<template>
+  <client-only>
+    <v-app>
+       <!-- class="mainbg" -->
+      <v-main class="bg">
+        <v-navigation-drawer
+          dark :height="($nuxt.$vuetify.breakpoint.name=='xs')?'80%':'100%'"
+          app
+          class="mainnav"
+          v-model="drawer"
+          :mini-variant="miniVariant"
+          :clipped="clipped"
+          :mini-variant-width="this.$auth.$state.loggedIn ? '60' : '0'"
+        >
+          <v-list v-if="this.$auth.$state.loggedIn">
+            <v-list-item to="/set/profile">
+              <v-list-item-avatar v-if="this.$auth.$state.loggedIn" size="36">
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-img contain :src="userPicture.picture"  v-bind="attrs" v-on="on"></v-img>
+                        <!-- <img src="../../assets/feedfish.jpg" alt="" v-bind="attrs" v-on="on" height="100"> -->
+                    </template>
+                    <span> {{ this.$auth.$state.user.name }}</span>
+                </v-tooltip>
+                <!-- <v-img contain :src="this.$auth.$state.user.picture" :title="this.$auth.$state.user.name" ></v-img> -->
+              </v-list-item-avatar>
+              <v-list-item-icon v-if="!this.$auth.$state.loggedIn">
+                <v-icon large>mdi-account-circle</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content v-if="this.$auth.$state.loggedIn" size="36">
+                <v-list-item-title class="h6">
+                  {{ this.$auth.$state.user.name }}
+                </v-list-item-title>
+                <v-list-item-subtitle class="caption">
+                  {{ this.$auth.$state.user.email }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider></v-divider>
+  
+            <!-- <v-list-item
+              v-for="(item, i) in listitems"
+              :key="i"
+              :to="item.to"
+              v-show="
+                ($auth.$state.user &&
+                  superaccount.includes($auth.$state.user.email) &&
+                   ['場域設定','觀察網影像'].includes(item.title)) ||
+                  !['場域設定','觀察網影像'].includes(item.title)
+              "
+            >
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item> -->
+          </v-list>
+          <sidelist :myitem="listitems" titleName="name" urlName="url" active="is_drop_down" v-if="listitems.length > 0"></sidelist>
+          <!--  -->
+        </v-navigation-drawer>
+        <v-container fluid>
+          <v-app-bar app class="mainnav"  dark>
+            <v-app-bar-nav-icon
+              @click.stop="drawer = !drawer"
+              v-show="this.$auth.$state.loggedIn"
+            ></v-app-bar-nav-icon>
+            <v-btn
+              icon
+              @click.stop="miniVariant = !miniVariant"
+              v-show="this.$auth.$state.loggedIn && drawer==true"
+            >
+              <v-icon
+                >mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon
+              >
+            </v-btn>
+            <nuxt-link to="/" style="color:white;text-decoration:none;"><v-icon>mdi-home-outline</v-icon><span class="hidden-sm-and-down">艾滴科技</span></nuxt-link>
+            <!-- <nuxt-link class="mx-3" to="/videorecog" style="color:white;text-decoration:none;"><v-icon>mdi-video</v-icon>觀察網影像辨識(測試)</nuxt-link> -->
+            <!-- <weather></weather>
+            <market-price class="hidden-sm-and-down"></market-price> -->
+            <v-spacer></v-spacer>
+            <!-- <v-btn
+              icon
+              to="/set/"
+              v-show="
+                this.$auth.$state.loggedIn &&
+                  superaccount.includes(this.$auth.$state.user.email)
+              "
+              ><v-icon>mdi-cog-outline</v-icon></v-btn
+            > -->
+            <weather></weather>
+            <market-price class="hidden-sm-and-down"></market-price>
+            <div v-if="false" class="switch-ip">
+              <v-switch class="mt-3 mx-1"
+                v-model="site" dense
+                @change="changeSite"
+              ><span class="hidden-sm-and-down" slot="label">{{(site)?'IP':'網域'}}</span></v-switch>
+            </div>
+            <div v-if="this.$auth.$state.loggedIn">
+              <!-- {{ this.$auth.$state.user.name }} -->
+              <v-btn icon @click="logout"><v-icon>mdi-home-export-outline</v-icon></v-btn>
+            </div>
+            <div v-show="!this.$auth.$state.loggedIn">
+              <v-btn icon to="/login"><v-icon>mdi-home-import-outline</v-icon></v-btn>
+            </div>
+          </v-app-bar>
+          <nuxt />
+        </v-container>
+        <v-footer fixed>
+        <Footer></Footer>
+      </v-footer>
+      </v-main>
+    </v-app>
+    </client-only>
+  </template>
+  
+  <script>
+  import _ from "lodash";
+  export default {
+    // async beforeCreate() {
+    //   //登入時判別身份分別導頁
+    //   if (this.$auth.$state.loggedIn) {
+    //     let acclist = [];
+    //     console.log("now api:"+this.$store.state.mydata.gobal_api.apiUrl);
+    //     var sitevalue = this.getSite();
+    //     debugger;
+    //     console.log("now api:"+this.$store.state.mydata.gobal_api.apiUrl);
+    //     await this.$axios
+    //       .get(`${this.$store.state.mydata.gobal_api.apiUrl}/user-access/account/`)//所有使用者的清單
+    //       .then(res => {
+    //         acclist = res.data;
+    //       });
+    //     var acc = acclist.filter(
+    //       x => x.username.toLowerCase() == this.$auth.$state.user.email.toLowerCase()
+    //     );
+    //     //登入成功
+    //     if (acc.length == 1) {
+    //       //增加身份判別---
+    //       const updatedUser = { ...this.$auth.user };
+    //       updatedUser.role = "user"; //允許登入的
+    //       updatedUser.authcheck = true; //授權可登入
+    //       // this.$auth.setUser(updatedUser); //會造成Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+    //       //--------------
+    //     } else {
+    //       //登入失敗
+    //       const updatedUser = { ...this.$auth.user };
+    //       updatedUser.role = "guest";
+    //       updatedUser.authcheck = false;
+    //       // this.$auth.setUser(updatedUser); //會造成Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+    //       this.$router.push({ name: "loginfail" });
+    //     }
+    //   }
+    // },
+    data() {
+      return {
+        drawer: false, //一開始有無顯示drawer
+        miniVariant: true, //凝结导航抽屉宽度，也接受**.sync**修饰符。这样，抽屉在点击时会重新打开(小寬度的模式)
+        clipped: false,
+        superaccount: [
+                    'jianwei.wen@idwater.com.tw',
+                    'jeff.wang@idwater.com.tw',//前驊
+                    'alex.chen@idwater.com.tw',//廷宇
+                    'steven.huang@idwater.com.tw',//璿中
+                    'eric.cheung@idwater.com.tw',//星希
+                    'eason.lin@idwater.com.tw',//奕昇
+                    'cf.chien@idwater.com.tw',//靖芳
+                  ],
+        site:false,//true 外網 false 內網
+        userPicture: this.$auth.$state.user
+      };
+    },
+    async mounted() {
+          var sitevalue = this.getSite();//return external or internal
+          this.site = (sitevalue == 'external');//判斷內外網路
+    },
+    
+    computed: {
+      listitems:function(){
+        var data = this.$store.state.mydata.listitems;
+        return _.cloneDeep(data);
+      }
+    },
+    // async mounted() {
+    //   let acclist = [];
+    //   var sitevalue = this.getSite();//return external or internal
+    //   this.site = (sitevalue == 'external');//判斷內外網路
+    //   if (this.$auth.$state.loggedIn) {
+    //     let accheader ={account:this.$auth.$state.user.email}
+    //     await this.$axios
+    //       .get(`${this.$store.state.mydata.gobal_api.apiUrl}/user-access/authorization-menu/`,{headers:accheader,timeout: 3000})//帳號被授權進入的項目
+    //       .then(res => {
+    //         acclist = res.data;
+    //       })
+    //       .catch(err => {
+    //         acclist=[];
+    //             // this.$toast.error("讀取api失敗：" + err.message, { duration: 2000 });
+    //             alert(`讀取api失敗：[${err.config.url}]_${err.message}`);
+    //           });
+    //   }
+    //   this.listitems = acclist;
+    //   console.log("被授權進入的項目",this.listitems);
+    // },
+    methods: {
+      logout: function() {
+        this.drawer = false;
+        $nuxt.$auth.logout();
+        this.$router.push('/login');
+      },
+      changeSite(){
+        if (this.site) {//true 外網
+          this.setSite("external");
+        }else{
+          this.setSite("internal");
+        }
+        window.location.reload();
+      }
+    }
+  };
+  </script>
+  <style lang="scss">
+  #app {
+    background: url('~/assets/bg_blue_papper.jpg')
+      repeat center center fixed !important;
+    background-size: cover;
+  }
+  .papper {
+    background: url('~/assets/bg_blue_papper.jpg')
+      repeat center center fixed !important;
+    background-size: cover;
+  }
+  .nuxt-link-active {
+    padding: 8px;
+    display: flex;
+    align-items: center;
+    .v-icon {
+      margin-right: 4px;
+    }
+  }
+  .v-btn__content {
+    .v-icon,.theme--dark.v-image {
+      margin-right: 4px;
+    }
+  }
+  .switch-ip .v-messages{
+     display: none;
+     height: 0;
+     font-size: 1px;
+  }
+  .switch-ip .v-input--dense > .v-input__control > .v-input__slot {
+      margin-bottom: 8px;
+  }
+  .bg {
+    background: var(--linear-gradient-dark, linear-gradient(179deg, #00324E 0.78%, #40657A 197.21%));
+    // background: linear-gradient(178deg, #00324E -29.51%, #40657A 140.55%);
+  }
+  </style>
