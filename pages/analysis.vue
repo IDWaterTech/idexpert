3d37f3eeb0bc62d9d30971c5a4df189725693137
wei10cool <x215033@yahoo.com.tw>
2023-12-01 15:53:15 +0800

    補回驗證頁面

--- pages/analysis.vue
+++ pages/analysis.vue
@@ -1487,7 +1487,7 @@ export default {
     }
   },
   async created() {
-    // await this._pageCheck(); //驗證頁面是否可檢視
+    await this._pageCheck(); //驗證頁面是否可檢視
     const agent = new https.Agent({
       rejectUnauthorized: false
     });
