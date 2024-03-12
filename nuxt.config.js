4620e02ef99ca58aba1d960adce39069d25085a1
hsushihya <shihya.hsu@idwater.com.tw>
2024-03-12 15:05:49 +0800

    user api整理

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,7 @@ export default {
     { src: "~/plugins/vue-particles.js", ssr: false },
     { src: "~/plugins/antdesign.js", ssr: false },//暫時不用
     { src: "~/plugins/service/basic.js", ssr: false },
+    { src: "~/plugins/service/user.js", ssr: false },
     // { src: '@/plugins/vue-video.js', ssr: false } //vue-flv-player
     // { src: "~/plugins/vue-preview", ssr: false }
     // { src: "~/plugins/chart", mode: 'client' },
