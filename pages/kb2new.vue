c22fe028f256508315acb97ddcf8acdb2184503c
hsushihya <shihya.hsu@idwater.com.tw>
2024-04-26 16:29:40 +0800

    調整標題底線

--- pages/kb2new.vue
+++ pages/kb2new.vue
@@ -3751,7 +3751,7 @@ export default {
                 }
                 
                 .title {
-                    border-bottom: 1px solid rgba(0,0,0,0.1);
+                    //border-bottom: 1px solid rgba(0,0,0,0.1);
                     .v-icon.v-icon {
                         font-size: 1.1rem;
                     }
@@ -3769,7 +3769,7 @@ export default {
                     display: flex;
                     align-items: center;
                     justify-content: flex-end;
-                    border-bottom: 1px solid rgba(0,0,0,0.1);
+                    // border-bottom: 1px solid rgba(0,0,0,0.1);
                     padding: 8px;
                     button {
                         width: 24px;
