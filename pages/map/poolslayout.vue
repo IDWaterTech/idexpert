cb835c48995df5a58b86d79bb55fda815feb5477
hsushihya <shihya.hsu@idwater.com.tw>
2024-05-30 09:19:50 +0800

    調整預設的池況name->name_ch

--- pages/map/poolslayout.vue
+++ pages/map/poolslayout.vue
@@ -392,20 +392,20 @@ export default {
         ]
       },
       statcolor: [
-          { name: "無", color: "#D3DCE1",id: 1 },
-          { name: "default", color: "#00273E" },
-          { name: "放養中", color: "#F1E78D",id: 4 },
-          { name: "放養中(鎖排汙)", color: "#CBAAE5",id: 32 },
-          { name: " 集中暫養", color: "#8DA0E5",id: 31},
-          { name: "尚未洗池", color: "#E8DDBF",id: 6 },
-          { name: "已清洗", color: "#A8E6DB",id: 7 },
-          { name: "蓄水中", color: "#D3B280",id: 8 },
-          { name: "蓄水完畢", color: "#A5D380",id: 9 },
-          { name: "消毒中", color: "#80D3AB",id: 10 },
-          { name: "做水中", color: "#C5E8E6",id: 11 },
-          { name: "預備放苗", color: "#83C9F0",id: 12 },
-          { name: "空池", color: "#BFDAE8",id: 3 },
-          { name: "養殖審核", color: "#D3808F",id: 33 }
+          { name_ch: "無", color: "#D3DCE1",id: 1 },
+          { name_ch: "default", color: "#00273E" },
+          { name_ch: "放養中", color: "#F1E78D",id: 4 },
+          { name_ch: "放養中(鎖排汙)", color: "#CBAAE5",id: 32 },
+          { name_ch: " 集中暫養", color: "#8DA0E5",id: 31},
+          { name_ch: "尚未洗池", color: "#E8DDBF",id: 6 },
+          { name_ch: "已清洗", color: "#A8E6DB",id: 7 },
+          { name_ch: "蓄水中", color: "#D3B280",id: 8 },
+          { name_ch: "蓄水完畢", color: "#A5D380",id: 9 },
+          { name_ch: "消毒中", color: "#80D3AB",id: 10 },
+          { name_ch: "做水中", color: "#C5E8E6",id: 11 },
+          { name_ch: "預備放苗", color: "#83C9F0",id: 12 },
+          { name_ch: "空池", color: "#BFDAE8",id: 3 },
+          { name_ch: "養殖審核", color: "#D3808F",id: 33 }
       ],
       maxCols: 12,
       editState: false, //編輯池況
@@ -491,11 +491,12 @@ export default {
   },
   methods: {
     getItemColor: function(data) {
-      // console.log('getItemColor',data);
+      console.log('getItemColor',data);
       if (data == "") {
         return "white";
       }
       let data2 = this.statcolor.filter(x => x.name_ch == data);
+      console.log(data2,this.statcolor);
       if (data2.length == 1) {
         return data2[0].color;
       } else {
