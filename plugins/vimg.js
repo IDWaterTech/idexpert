5ec9b5f51869178b90aed14bcc7572e5a9daae1d
wei10cool <x215033@yahoo.com.tw>
2024-09-26 11:36:56 +0800

    開放下載鈕、縮圖(超過一張圖的才有)

--- plugins/vimg.js
+++ plugins/vimg.js
@@ -4,10 +4,10 @@ const vueImgConfig = {
     // Use `alt` attribute as gallery slide title
     altAsTitle: true,
     // Display 'download' button near 'close' that opens source image in new tab
-    // sourceButton: false,
+    sourceButton: true,
     // Event listener to open gallery will be applied to <img> element
     // openOn: 'click',
     // Show thumbnails for all groups with more than 1 image
-    // thumbnails: false,
+    thumbnails: true,
   }
 Vue.use(VueImg,vueImgConfig);
